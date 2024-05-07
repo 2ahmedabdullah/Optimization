876d0f85fde304f374701f8506fd35cc088378c5
Abdul Ahmed <2ahmedabdullah@gmail.com>
2024-05-07 15:25:50 +0530

    Add files via upload

---
+++ linear_optimization_cplex_gurobi_glpk.py
@@ -0,0 +1,41 @@
+import pyomo.environ as pyo
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+model = pyo.ConcreteModel()
+
+# defining bounds 0<= x <= 10
+model.x = pyo.Var(bounds=(0,10))
+
+# defining bounds 0<= y <= 10
+model.y = pyo.Var(bounds=(0,10))
+
+
+x = model.x
+y = model.y
+
+# Constraint1: -x+2y <= 8
+model.C1 = pyo.Constraint(expr= -x+2*y<=8)
+
+# Constraint2: 2x+2y <= 14
+model.C2 = pyo.Constraint(expr= 2*x+y<=14)
+
+# Constraint3: 2x-y <= 10
+model.C3 = pyo.Constraint(expr= 2*x-y<=10)
+
+# Objective function Z = x + y
+model.obj = pyo.Objective(expr= x+y, sense=maximize)
+
+#opt = SolverFactory('cplex')
+#opt = SolverFactory('gurobi')
+opt = SolverFactory('glpk')
+opt.solve(model)
+
+model.pprint()
+
+x_value = pyo.value(x)
+y_value = pyo.value(y)
+
+print('\n---------------------------------------------------------------------')
+print('x=',x_value)
+print('y=',y_value)
